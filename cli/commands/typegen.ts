d0e03ce0dc8246efc6ee1514d64e1bc2b05d5b62
Philipp Giese <giese.philipp+git@gmail.com>
2025-11-06 09:38:51 +0100

    create vault enum

--- cli/commands/typegen.ts
+++ cli/commands/typegen.ts
@@ -1,7 +1,27 @@
 import { InternalApiClient } from '../internalApi'
+import * as t from '@babel/types'
+import { generate } from '@babel/generator'
 
 export const typegen = async () => {
   const client = new InternalApiClient()
 
   const vaults = await client.listVaults()
+
+  const vaultsEnum = generate(
+    t.exportNamedDeclaration(
+      t.enumDeclaration(
+        t.identifier('Vault'),
+        t.enumStringBody(
+          vaults.map((vault) =>
+            t.enumStringMember(
+              t.identifier(vault.label.replaceAll(/ /g, '_')),
+              t.stringLiteral(vault.id)
+            )
+          )
+        )
+      )
+    )
+  )
+
+  console.log(vaultsEnum)
 }
--- cli/internalApi.ts
+++ cli/internalApi.ts
@@ -1,7 +1,8 @@
 import { ApiClient } from '../src'
+import { ListVaultsResult } from '@zodiac-os/api-types'
 
 export class InternalApiClient extends ApiClient {
-  listVaults() {
+  listVaults(): Promise<ListVaultsResult> {
     return this.get('vaults')
   }
 }
